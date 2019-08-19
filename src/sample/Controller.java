c2fef4cf603a326f87aec4c8fcb05844095b2588
Philip Schneider <philipschneider42@gmail.com>
2019-08-19 16:00:08 +0200

    Added TODOs

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
 public class Controller {
+    // TODO Implement Chat
+
+    // TODO Implement GUI via JavaFX
+    // TODO Find Users via TCP
+    // TODO Encrypt Messages
+    // TODO Send Messages via TCP to Every User
+    // TODO Filter Messages
+    // TODO Save Messages in Database
+    // TODO Ping for Online-Status
+    // TODO Status und Profilbild
+    // TODO Windows-Notifications
+
+    // TODO Show Network Graphically
+
+
+    //
+    //
+    // TCP
+    // Nutzer finden über Netzwerknachrichten
+    // Ping -- Online?
+    //
 }
