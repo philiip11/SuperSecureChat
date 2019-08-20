32cdf276e032819913540a0d6dd68e1ddc40b7d7
Voelz <blauesding2@gmx.de>
2019-08-20 11:20:33 +0200

    TrayIcon cleanup

--- src/SuperSecureChat/TrayIconDemo.java
+++ src/SuperSecureChat/TrayIconDemo.java
@@ -5,28 +5,25 @@ import java.awt.TrayIcon.MessageType;
 
 public class TrayIconDemo {
 
-    public static void main(String[] args) throws AWTException {
-        if (SystemTray.isSupported()) {
-            TrayIconDemo td = new TrayIconDemo();
-            td.displayTray();
-        } else {
-            System.err.println("System tray not supported!");
-        }
-    }
+//    public static void test() throws AWTException {
+//        if (SystemTray.isSupported()) {
+//            TrayIconDemo td = new TrayIconDemo();
+//            td.displayTray();
+//        } else {
+//            System.err.println("System tray not supported!");
+//        }
+//    }
 
     public void displayTray() throws AWTException {
-        //Obtain only one instance of the SystemTray object
+
         SystemTray tray = SystemTray.getSystemTray();
 
-        //If the icon is a file
         Image image = Toolkit.getDefaultToolkit().createImage("icon.png");
-        //Alternative (if the icon is on the classpath):
-        //Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("icon.png"));
 
         TrayIcon trayIcon = new TrayIcon(image, "Tray Demo");
-        //Let the system resize the image if needed
+
         trayIcon.setImageAutoSize(true);
-        //Set tooltip text for the tray icon
+
         trayIcon.setToolTip("System tray icon demo");
         tray.add(trayIcon);
 
