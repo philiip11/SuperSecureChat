aa7f06e334f93c52af3c6dcd9b884407f3b86cd3
Voelz <blauesding2@gmx.de>
2019-08-20 10:38:04 +0200

    added TODO document

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,30 +1,5 @@
 package sample;
 
 public class Controller {
-    // TODO Implement Chat
 
-    // TODO Implement GUI via JavaFX
-    // TODO Find Users via TCP
-    // TODO Encrypt Messages (just the user the message was meant for can read it)
-    // TODO Send Messages via TCP to Every User
-    // TODO Filter Messages
-    // TODO Save Messages in Database
-    // TODO Ping for Online-Status (cmd ping like request to see if one is online, [online/offline])
-    // TODO Status und Profilbild
-
-
-    // TODO Windows-Notifications   (found some awt guide, not javafx tho)
-
-
-    // TODO Show Network Graphically (transport of sent messages)
-
-
-    //TODO Filter for specific words (Swear words --> *****) [List for words who will be censored?]
-
-    //
-    //
-    // TCP
-    // Nutzer finden über Netzwerknachrichten
-    // Ping -- Online?
-    //
 }
