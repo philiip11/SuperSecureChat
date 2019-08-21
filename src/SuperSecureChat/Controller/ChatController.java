f1f78cbe259bf9fa2c10cf2bffded11f0eb64055
Voelz <blauesding2@gmx.de>
2019-08-21 09:17:16 +0200

    setup button 'on Action' code

--- src/SuperSecureChat/Controller/ChatController.java
+++ src/SuperSecureChat/Controller/ChatController.java
@@ -1,8 +1,14 @@
 package SuperSecureChat.Controller;
 
 import SuperSecureChat.Contacts.Contact;
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
 
 public class ChatController {
+
+    @FXML
+    JFXButton sendMessage;
+
     private Contact contact;
 
     public void setContact(Contact contact) {
@@ -10,6 +16,10 @@ public class ChatController {
     }
 
     public void initialize() {
+
+    }
+
+    public void buttonClick() {
 
     }
 }
