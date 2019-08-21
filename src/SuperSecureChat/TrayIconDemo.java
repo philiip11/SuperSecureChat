1e63b010dc6ca813c55ae70beffcfa21c004e86c
Philip Schneider <philipschneider42@gmail.com>
2019-08-21 08:10:47 +0200

    Fixed Andreas' Bug

--- src/SuperSecureChat/TrayIconDemo.java
+++ src/SuperSecureChat/TrayIconDemo.java
@@ -5,13 +5,13 @@ import javafx.application.Platform;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.TrayIcon.MessageType;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 public class TrayIconDemo {
-    public static final String PATH_TO_TRAY_ICON = "C:\\Users\\voelz\\Downloads\\messengericon.jpg";
+    //public static final String PATH_TO_TRAY_ICON = "C:\\Users\\voelz\\Downloads\\messengericon.jpg"; // ARGH! Niemals absolute Pfade nutzen,
+    // das läuft ja dann nur auf Sebastian's PC
 
+    public static final String PATH_TO_TRAY_ICON = "/messengericon.jpg";
     public static void test() throws AWTException {
         if (SystemTray.isSupported()) {   //frägt ab ob SystemTray vom System unterstützt wird
             TrayIconDemo td = new TrayIconDemo();
@@ -45,14 +45,26 @@ public class TrayIconDemo {
 
     }
 
-    private BufferedImage getTrayIcon() {
-        File f = new File(PATH_TO_TRAY_ICON);
-        BufferedImage bi = null;
+
+//    private BufferedImage getTrayIcon() {
+//        File f = new File(PATH_TO_TRAY_ICON);
+//        BufferedImage bi = null;
+//        try {
+//            bi = ImageIO.read(f);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return bi;
+//    }
+
+    // Besser das Bild aus den Resourcen laden.
+
+    private Image getTrayIcon() {
         try {
-            bi = ImageIO.read(f);
+            return ImageIO.read(getClass().getResource(PATH_TO_TRAY_ICON));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return bi;
+        return null;
     }
 }
