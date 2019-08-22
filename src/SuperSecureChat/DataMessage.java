c4c6ad68c2893d38a0052872f74f280b6962db58
Philip Schneider <philipschneider42@gmail.com>
2019-08-22 13:51:24 +0200

    Typo

--- src/SuperSecureChat/DataMessage.java
+++ src/SuperSecureChat/DataMessage.java
@@ -3,7 +3,7 @@ package SuperSecureChat;
 import SuperSecureChat.Contacts.Contact;
 
 public class DataMessage extends Message {
-    public DataMessage(String id, String referencId, Contact sender, Contact reciever, String text, String data, String trace, int created, int read, int received) {
-        super(id, referencId, sender, reciever, text, data, trace, created, read, received);
+    public DataMessage(String id, String referencId, Contact sender, Contact receiver, String text, String data, String trace, int created, int read, int received) {
+        super(id, referencId, sender, receiver, text, data, trace, created, read, received);
     }
 }
--- src/SuperSecureChat/Message.java
+++ src/SuperSecureChat/Message.java
@@ -10,7 +10,7 @@ public class Message {
     private String id;                  // Eindeutige ID der Nachricht, bestehend aus Nutzername des Senders und seinem lokalen Counter
     private String referenceId;          // Referenz auf andere Nachricht, bspw. für Antwort oder Korrektur
     private Contact sender;             // Absender
-    private Contact reciever;           // Empfänger
+    private Contact receiver;           // Empfänger
     private String text;                // Nachrichttext
     private String data;                // ggf. Datei
     private String trace;               // Spur, über welche PCs ist die Nachricht geschickt (Nutzername des PCs, Uhrzeit, IP?)
@@ -18,11 +18,11 @@ public class Message {
     private int read;                   // Nachricht gelesen?  1 = ja, 0 = nein
     private int received;               // Nachricht beim Empfänger angekommen?
 
-    public Message(String id, String referenceId, Contact sender, Contact reciever, String text, String base64data, String trace, long created, int read, int received) {
+    public Message(String id, String referenceId, Contact sender, Contact receiver, String text, String base64data, String trace, long created, int read, int received) {
         this.id = id;
         this.referenceId = referenceId;
         this.sender = sender;
-        this.reciever = reciever;
+        this.receiver = receiver;
         this.text = text;
         this.data = base64data;
         this.trace = trace;
@@ -35,13 +35,40 @@ public class Message {
 
     }
 
+    public static Message fromJSON(String json) {
+        Message message = new Message();
+        try {
+            JSONObject jsonObject = (JSONObject) new JSONParser().parse(json);
+            message.setId(jsonObject.get("id").toString());
+            message.setReferencId(jsonObject.get("referenceID").toString());
+            message.setSender(Contact.fromJSON(jsonObject.get("sender").toString()));
+            message.setreceiver(Contact.fromJSON(jsonObject.get("receiver").toString()));
+            message.setText(jsonObject.get("text").toString());
+            message.setData(jsonObject.get("data").toString());
+            message.setTrace(jsonObject.get("trace").toString());
+            message.setCreated((long) jsonObject.get("created"));
+            message.setRead((int) jsonObject.get("read"));
+            message.setReceived((int) jsonObject.get("recieved"));
+            message.setId(jsonObject.get("id").toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return message;
+    }
+
+    public String toJSONString() {
+        return toJSON().toString();
+
+    }
+
     @Override
     public String toString() {
         return "Message{" +
                 "id='" + id + '\'' +
                 ", referenceId='" + referenceId + '\'' +
                 ", sender=" + sender +
-                ", reciever=" + reciever +
+                ", receiver=" + receiver +
                 ", text='" + text + '\'' +
                 ", data=" + data +
                 ", trace='" + trace + '\'' +
@@ -51,18 +78,13 @@ public class Message {
                 '}';
     }
 
-    public String toJSONString() {
-        return toJSON().toString();
-
-    }
-
     @SuppressWarnings({"unchecked", "WeakerAccess"})
     public JSONObject toJSON() {
         JSONObject jsonMessage = new JSONObject();
         jsonMessage.put("id", id);
         jsonMessage.put("referenceID", referenceId);
         jsonMessage.put("sender", sender.toJSON());
-        jsonMessage.put("reciever", reciever.toJSON());
+        jsonMessage.put("receiver", receiver.toJSON());
         jsonMessage.put("text", text);
         jsonMessage.put("data", data);
         jsonMessage.put("trace", trace);
@@ -70,28 +92,6 @@ public class Message {
         jsonMessage.put("read", read);
         jsonMessage.put("received", received);
         return jsonMessage;
-    }
-
-    public static Message fromJSON(String json) {
-        Message message = new Message();
-        try {
-            JSONObject jsonObject = (JSONObject) new JSONParser().parse(json);
-            message.setId(jsonObject.get("id").toString());
-            message.setReferencId(jsonObject.get("referenceID").toString());
-            message.setSender(Contact.fromJSON(jsonObject.get("sender").toString()));
-            message.setReciever(Contact.fromJSON(jsonObject.get("receiver").toString()));
-            message.setText(jsonObject.get("text").toString());
-            message.setData(jsonObject.get("data").toString());
-            message.setTrace(jsonObject.get("trace").toString());
-            message.setCreated((long) jsonObject.get("created"));
-            message.setRead((int) jsonObject.get("read"));
-            message.setReceived((int) jsonObject.get("received"));
-            message.setId(jsonObject.get("id").toString());
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return message;
     }
 
     public String getId() {
@@ -118,12 +118,12 @@ public class Message {
         this.sender = sender;
     }
 
-    public Contact getReciever() {
-        return reciever;
+    public Contact getreceiver() {
+        return receiver;
     }
 
-    private void setReciever(Contact reciever) {
-        this.reciever = reciever;
+    private void setreceiver(Contact receiver) {
+        this.receiver = receiver;
     }
 
     public String getText() {
